7aa00d1cf8eaa8c7b337dcea75b2359da410fc4f
Urahara <17600340273@163.com>
2026-02-01 19:53:53 +0900

    Configure GitHub Pages

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
-export const repositoryName = 'MarquezSpace'; // "https://alancumberbatch.github.io/MarquezSpace/",
+export const repositoryName = 'Noah_Page'; // "https://alancumberbatch.github.io/Noah_Page/",
 // export const repositoryName = ''; // "https://alancumberbatch.github.io/",
 // repositoryName: 这里改完之后， package.json 里的 也需要改一下
